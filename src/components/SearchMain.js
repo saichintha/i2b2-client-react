6133dc79deda2c0c063165c024f431bc25d654e9
Sai Chintha <saichinthas@gmail.com>
2017-07-13 22:19:19 -0500

    Added Snackbar for adding, removing concepts.

--- src/components/SearchMain.js
+++ src/components/SearchMain.js
@@ -2,25 +2,64 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import HierarchyView from './Search/HierarchyView';
 import SearchResultList from './Search/SearchResultList';
-
-const padding = '5em';
+import Snackbar from 'material-ui/Snackbar';
+import * as actions from './../redux/actions.js'
+import {connect} from 'react-redux';
 
 class SearchMain extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      open: false,
+      msg: ''
+    }
+  }
+
+  handleRequestClose = () => {
+    this.setState({
+      open: false,
+    });
+  };
+
+  componentWillReceiveProps (nextProps) {
+    this.setState({
+      open: nextProps.snackBar.open,
+      concept: nextProps.snackBar.concept,
+      msg: nextProps.snackBar.prefix + nextProps.snackBar.concept,
+      elementID: nextProps.snackBar.elementID
+    })
+  }
+
+  handleActionTouchTap = () => {
+    var {dispatch} = this.props;
+    var {concept} = this.state;
+    dispatch(actions.removeConceptFromGroup(this.state.elementID));
+    dispatch(actions.openSnackBar(true, 'Removed – ', concept, 'aa'))
+  }
+
   render() {
     return (
-      <div className="row center-xs" style={{paddingTop: 30}}>
-        {/*<div className="col-xs-5" style={{paddingLeft: 20, paddingRight: 12, overflowY: 'hidden'}}>
-            <HierarchyView />
-        </div>*/}
-
+      <div className="row center-xs" style={{paddingTop: 30, paddingBottom: 50}}>
         <div className="col-xs-10" >
             <SearchResultList />
         </div>
 
+        <Snackbar
+          open={this.state.open}
+          message={this.state.msg}
+          action="undo"
+          autoHideDuration={4000}
+          onActionTouchTap={this.handleActionTouchTap}
+          onRequestClose={this.handleRequestClose}
+        />
 
       </div>
     );
   }
 }
 
-export default SearchMain;
+export default connect((state) => {
+  return {
+    snackBar: state.snackBar
+  }
+})(SearchMain);
